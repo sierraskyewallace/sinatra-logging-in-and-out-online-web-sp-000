62a5d818634e11d29ca57c0fbb483b64159bd909
Sierra Wallace <sskwallace@gmail.com>
2021-01-31 04:34:53 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,12 +22,7 @@ class ApplicationController < Sinatra::Base
     
 
   get '/account' do
-    @current_user = User.find_by_id(session[:user_id])
-    if @current_user
-      erb :account
-    else
-      erb :error
-    end
+    erb :account
   end	
 
   get '/logout' do
